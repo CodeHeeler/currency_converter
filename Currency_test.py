e35c2f164c8c91e46cf346cff9c4b60d4d95703b
RebelMerf <rebelmurf@gmail.com>
2017-02-27 21:57:54 -0500

    began testing

--- Currency_test.py
+++ Currency_test.py
@@ -1,9 +1,15 @@
 from Currency import Currency
 
 
+five_dollars = Currency("$5")
+my_money = Currency("5", "USD")
+
+def test_print():
+    assert print(five_dollars) == "$5"
 
 def test_is_equal():
-    assert
+    assert (my_money == five_dollars) == True
 
 
-test_    ()
+test_print()
+test_is_equal()
